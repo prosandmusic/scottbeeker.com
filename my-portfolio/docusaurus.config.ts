bf9f8ecd423fc40bbac56ba326c00074d66e0300
bayareadevlead@gmail.com <bayareadevlead@gmail.com>
2024-12-22 18:59:43 -0800

    work in progress

--- my-portfolio/docusaurus.config.ts
+++ my-portfolio/docusaurus.config.ts
@@ -1,44 +1,166 @@
-import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
 
+const config: Config = {
+  title: 'Scott Beekers',
+  tagline: 'Engineering Blog and Courses',
+  favicon: 'img/favicon.ico',
 
-const sidebars: SidebarsConfig = {
-  EngineeringTutorialsSidebar: [
-    'Tutorials/Development-Tutorials',
+ 
+  url: 'https://scottbeeker.info',
+  
+  baseUrl: '/',
 
-    {
-      type: 'category',
-      label: 'Tutorials',
-      items: ['Tutorials/Development-Tutorials',
-              'Tutorials/Softskills-Tutorials',
-      ],
-    },
+  
+  organizationName: 'Scott A Beeker', // Usually your GitHub org/user name.
+  projectName: 'Scott Beeker Info', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+  
+          },
+        blog: {
+          showReadingTime: true,
+  
+          },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
   ],
 
-  ResearchSidebar: [
-    'Research/Fediverse',
-    'Research/OperatingSystems',
-    'Research/CustomWearables',
-    'Research/Machinelearning',
-    'Research/ConsumerTrends',
-    {
-      type: 'category',
-      label: 'Fediverse',
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/logo.jpeg',
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true
+    },
+
+
+
+    announcementBar: {
+      id: 'comingsoon',
+      content: 'Courses coming soon!'
+    },
+
+
+    navbar: {
+      title: 'Scott Beeker',
+      logo: {
+        alt: '',
+        src: 'img/logo.jpeg',
+      },
       items: [
-        'Research/Fediverse/ActivityPub',
-        'Research/Fediverse/ATProtocol',
-        'Research/Fediverse/ActivityPub2',
+        {
+          type: 'docSidebar',
+          sidebarId: 'EngineeringTutorialsSidebar',
+          position: 'left',
+          label: 'Tutorials',
+        },
+
+        {
+          type: 'docSidebar',
+          sidebarId: 'ResearchSidebar',
+          position: 'left',
+          label: 'Research'
+      },
+      
+      {
+
+          to: '/projects',
+          label: 'Projects',
+          position: 'left', },
+        {
+          
+          to: '/blog', 
+          label: 'Blog', 
+          position: 'left'},
+        
       ],
     },
-    {
-      type: 'category',
-      label: 'ConsumerTrends',
-      items: [
-        'Research/ConsumerTrends/Music',
-        'Research/ConsumerTrends/Retail',
-        'Research/ConsumerTrends/Fashion',
-        'Research/ConsumerTrends/Work',
+
+
+
+
+    footer: {
+      links: [
+        { label: 'X', href: 'https://x.com/mescottbeeker' },
+        { label: 'Linkedin', href: 'https://Linkedin.com/in/scottbeekerofficial' },
+        { label: 'Github', href: 'https://github.com/prosandmusic' },
       ],
+      copyright: 'Copyright © 2025 Scott Beeker.',
     },
-  ],
+        
+      
+      
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
 };
+
+export default config;
+
+
+// import {themes as prismThemes} from 'prism-react-renderer';
+// import type {Config} from '@docusaurus/types';
+// import type * as Preset from '@docusaurus/preset-classic';
+
+// const sidebars: SidebarsConfig = {
+//   EngineeringTutorialsSidebar: [
+//     'Tutorials/Development-Tutorials',
+
+//     {
+//       type: 'category',
+//       label: 'Tutorials',
+//       items: ['Tutorials/Development-Tutorials',
+//               'Tutorials/Softskills-Tutorials',
+//       ],
+//     },
+//   ],
+
+//   ResearchSidebar: [
+//     'Research/Fediverse',
+//     'Research/OperatingSystems',
+//     'Research/CustomWearables',
+//     'Research/Machinelearning',
+//     'Research/ConsumerTrends',
+//     {
+//       type: 'category',
+//       label: 'Fediverse',
+//       items: [
+//         'Research/Fediverse/ActivityPub',
+//         'Research/Fediverse/ATProtocol',
+//         'Research/Fediverse/ActivityPub2',
+//       ],
+//     },
+//     {
+//       type: 'category',
+//       label: 'ConsumerTrends',
+//       items: [
+//         'Research/ConsumerTrends/Music',
+//         'Research/ConsumerTrends/Retail',
+//         'Research/ConsumerTrends/Fashion',
+//         'Research/ConsumerTrends/Work',
+//       ],
+//     },
+//   ],
+// };
 
